3d46261a08dbf412215661023b4fd085685170b7
Takaharu Nakamura <kaibatsu35.7m45@gmail.com>
2024-12-22 09:12:33 +0900

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use serde::Deserialize;
 use std::{
     env::{self},
     fs::{File, Permissions},
@@ -8,24 +9,42 @@ use std::{
     str::FromStr,
 };
 use tokio::signal;
+use toml::from_str;
 
 use base64::prelude::*;
 use reqwest::{redirect::Policy, Client, IntoUrl, Url};
 use tokio::fs::{self, create_dir};
 use tokio::sync::mpsc;
 
+#[derive(Deserialize)]
+struct Config {
+    repo: String,
+    binary_name: String,
+    resource: Resouce,
+    config_path: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct Resouce {
+    service_name: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pwd = env::current_dir().unwrap();
+    let config_path = pwd.join("config.toml");
+    if !config_path.exists() {
+        ()
+    }
+    let config = from_str::<Config>(fs::read_to_string(config_path).await?.as_str())?;
     let binary_path = find_binary_root(&pwd);
     if !binary_path.exists() {
         create_dir(&binary_path).await?;
     }
-    let binary_row_name = "my-otel-collector-macos-14";
-    let latest_url: Url =
-        Url::parse("https://github.com/appare45/my-otel-collector/releases/latest")?;
+    let latest_url: Url = Url::parse(format!("{}/releases/latest", config.repo).as_str())?;
     let latest_release_url = get_latest_version_url(latest_url).await?;
-    let download_url = format!("{}/{}", latest_release_url, binary_row_name);
+    let download_url = format!("{}/{}", latest_release_url, config.binary_name);
+
     let download_url =
         Url::from_str(str::replace(download_url.as_str(), "tag", "download").as_str())?;
     let binary_id = BASE64_URL_SAFE.encode(md5::compute(download_url.as_str()).as_ref());
@@ -36,14 +55,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut bin = File::create(&binary_path)?;
         bin.write(bytes.as_ref())?;
     }
-    debug_assert!(binary_path.exists());
     fs::set_permissions(&binary_path, Permissions::from_mode(0o700)).await?;
     let (_, mut shutdown_recv) = mpsc::unbounded_channel::<i32>();
     let row_path = binary_path.to_str().unwrap_or("");
     let mut command = Command::new(row_path);
     command
-        .env("OTEL_RESOURCE_ATTRIBUTES", "service.name=macosprivate")
-        .args(["--config", "./config.yaml"]);
+        .env(
+            "OTEL_RESOURCE_ATTRIBUTES",
+            format!(
+                "service.name={}",
+                config.resource.service_name.unwrap_or_default()
+            ),
+        )
+        .args([
+            "--config",
+            config
+                .config_path
+                .unwrap_or(String::from("./config.yaml"))
+                .as_str(),
+        ]);
     if let Ok(mut child) = command.spawn() {
         println!("started {}", child.id());
         tokio::select! {
@@ -73,6 +103,9 @@ async fn get_latest_version_url<T: IntoUrl>(url: T) -> Result<Url, Box<dyn std::
         if let Some(location) = response.headers().get("Location") {
             return Ok(Url::from_str(location.to_str()?)?);
         }
+    }
+    if !response.status().is_success() {
+        ()
     }
     return Ok(response.url().clone());
 }
